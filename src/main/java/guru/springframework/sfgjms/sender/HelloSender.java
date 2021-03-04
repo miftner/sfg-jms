61423d8cd47691e2014f6cdfffbce267539f1c9d
Maximilian Iftner <maximilianiftner@gmail.com>
2021-03-04 13:42:18 +0100

    Adding an Sender-BL to utilize an preconfigured jmsTemplate and our Pojo-Class to send an Message every 2s to our queue

--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -1,17 +1,34 @@
 package guru.springframework.sfgjms.sender;
 
 
+import guru.springframework.sfgjms.configuration.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Component
 public class HelloSender {
 
+    private final JmsTemplate jmsTemplate;
+
     @Scheduled(fixedRate = 2000)
-    public void sendMessage(){
+    public void sendMessage() {
 
         System.out.println("I'm sending an automated mesage");
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello World!")
+                .build();
 
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message Sent!");
     }
 
 }
