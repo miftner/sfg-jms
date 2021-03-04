04d4cb1702a23a74d11c3dcdc94ed5da7791003f
Maximilian Iftner <maximilianiftner@gmail.com>
2021-03-04 13:25:38 +0100

    Adding an scheduled Task and an default-Queuename

---
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgjms.sender;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloSender {
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+
+        System.out.println("I'm sending an automated mesage");
+
+    }
+
+}
